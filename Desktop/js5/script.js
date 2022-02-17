33375ca203f64bd909d9541f62f8f765787015a1
Yuka <yuaaasly116@icloud.com>
2022-02-17 23:37:36 +0900

    オーバーライド

---
+++ Desktop/js5/script.js
@@ -0,0 +1,41 @@
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name,age,breed){
+    super(name,age);
+    this.breed = breed;
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`犬種は${this.breed}です`);
+    
+    console.log(`${this.age}歳です`);
+    const humanAge = this.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
+  }
+  
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+const dog = new Dog("レオ", 4,"チワワ");
+dog.info();
+
